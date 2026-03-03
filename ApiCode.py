346facdc3ce44d96f4b8a14be15d6e2da96d2c2f
Olajide Olatokunboh <165014534+OLATOKS@users.noreply.github.com>
2026-03-03 01:53:58 +0100

    Update ApiCode.py with the Get routes with path parameters  and type validation

--- ApiCode.py
+++ ApiCode.py
@@ -13,7 +13,3 @@ def about():
 @app.get("/user/{user_id}")
 def get_user(user_id: int):
     return {"user_id": user_id}
-
-#@app.post("/tk")
-#def yeahhhhhhhh(data: dict):
-    #return {"you_sent": data}
