56cf1cd2ab4221b114fe825974e051d215898b75
Olajide Olatokunboh Olayinka <olajidetokunboh@gmail.com>
2026-03-09 14:17:01 +0100

    this stores users in memory and also get users

--- ApiCode.py
+++ ApiCode.py
@@ -1,19 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+users = []
+
+class User(BaseModel):
+    name: str
+    age: int
+    is_admin : bool = False
 
 @app.get("/")
 def home():
     return{"message":"AI backend is running"}
 
-@app.get("/about")
-def about():
-    return {"message": "This is the AI backend foundation project"}
+
+@app.post("/create-user")
+def create_user(user:User):
+    users.append(user)
+    return{
+        "message": "user created successfully",
+        "user":user
+    }
+
+@app.get("/users")
+def get_all_users():
+    return {"users": users}
 
 @app.get("/user/{user_id}")
 def get_user(user_id: int):
-    return {"user_id": user_id}
-
-#@app.post("/tk")
-#def yeahhhhhhhh(data: dict):
-    #return {"you_sent": data}
+    try:
+        return {"user": users[user_id]}
+    except IndexError:
+        return {"error": "User not found"}
